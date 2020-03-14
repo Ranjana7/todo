6a15becddc9fc6d798af9012103cee0cf5d0abb6
Ranjana Sharma <ranjana.sharma@fmr.com>
2020-03-14 18:43:40 +0000

    Delete old

--- todo-service/src/main/java/com/test/deloitte/dao/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.test.deloitte.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.test.deloitte.model.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
